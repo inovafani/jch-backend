9b06e89f5e444e4b494906700b0c7637fab8c73c
inovafani <inoveka@gmail.com>
2025-08-12 00:57:33 +0700

    Add start.sh

---
+++ start.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+composer install --no-dev --optimize-autoloader
+php artisan config:clear
+php artisan route:clear
+php artisan view:clear
+chmod -R 775 storage bootstrap/cache
+php artisan serve --host=0.0.0.0 --port=${PORT}
