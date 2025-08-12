9d11a9f48fc9661f0c616e8d1c3afe9c06b936c5
inovafani <inoveka@gmail.com>
2025-08-12 09:50:07 +0700

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,19 +1,14 @@
 #!/bin/bash
 
-# Jalankan migrate
+# Pastikan folder view & cache ada
+mkdir -p storage/framework/views
+mkdir -p bootstrap/cache
+
+# Set permission
+chmod -R 775 storage bootstrap/cache storage/framework/views
+
+# Migrasi database (pakai force biar jalan di production)
 php artisan migrate --force
 
-# Set permission storage dan cache
-chmod -R 775 storage bootstrap/cache
-
-# Clear dan cache ulang config, route, view
-php artisan config:clear
-php artisan cache:clear
-php artisan route:clear
-php artisan view:clear
-php artisan config:cache
-php artisan route:cache
-php artisan view:cache
-
-# Jalankan Laravel di host dan port Railway
+# Jalankan server Laravel
 php artisan serve --host=0.0.0.0 --port=${PORT}
